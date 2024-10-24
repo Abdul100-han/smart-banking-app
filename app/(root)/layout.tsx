a648ac0b3e5959a7b75778894c182d51a1c816a3
Abdul100-han <abdulmuhd857@gmail.com>
2024-10-24 20:32:13 +0100

    imlement the complete layout

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -1,13 +1,31 @@
+import MobilNav from "@/components/MobilNav";
+import Sidebar from "@/components/Sidebar";
+import Image from "next/image";
 
 export default function RootLayout({
   children,
 }: Readonly<{
   children: React.ReactNode;
 }>) {
+
+  const loggedIn = { firstName: 'Abdul', lastName: 'Muhammad' }
+
   return (
-    <main>
-        SIDEBAR
-        {children}
+    <main className="flex h-screen w-full">
+        <Sidebar
+        user={loggedIn}
+        />
+
+        <div className="flex size-full flex-col">
+          <div className="root-layout">
+            <Image src='/icons/logo.svg' width={30} height={30} alt="logo" />
+          <div>
+            <MobilNav user={loggedIn} />
+          </div>
+          </div>
+          {children}
+        </div>
+        
     </main>
   );
 }
--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,10 +1,39 @@
+import HeaderBox from '@/components/HeaderBox'
+import RightSidebar from '@/components/RightSidebar'
+import TotalBalanceBox from '@/components/TotalBalanceBox'
 import React from 'react'
 
 const page = () => {
+
+  const loggedIn = {firstName: 'Abdul', lastname:'Muhammad', email:'abdul@gmail.com'}
+
   return (
-    <div>
-      Dashboard
-    </div>
+    <section className='home'>
+      <div className="home-content">
+        <header className="home-header">
+          <HeaderBox
+          type="greeting"
+          title="Welcome"
+          user={loggedIn.firstName || 'Guest'}
+          subtext="Access and manage your account and transactions efficeintly"
+          />
+
+          <TotalBalanceBox 
+          accounts={[]}
+          totalBanks={1}
+          totalCurrentBalance={1250.35}
+          />
+        </header>
+
+        RECENT TRANSACTION
+      </div>
+
+      <RightSidebar
+      user={loggedIn}
+      transactions={[]}
+      banks={[{currentBalance: 123.50}, {currentBalance: 500.50}]}
+      />
+    </section>
   )
 }
 
